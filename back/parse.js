51f4cb80e4569876e261b1111c58f1150937609e
Beka Sturua <beqasturua3@gmail.com>
2022-12-06 23:14:55 +0400

    feat: configure supabase

---
+++ back/parse.js
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+const { data, error } = await supabase.from("data").select();
+
+console.log(data);
