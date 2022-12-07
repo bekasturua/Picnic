46c87d1688d50e0e57e6637018b84b7091254a5b
Beka Sturua <beqasturua3@gmail.com>
2022-12-07 17:35:32 +0400

    add front

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,52 @@
 import "./App.css";
+import DataTable from "react-data-table-component";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const columns = [
+  {
+    name: "Question",
+    selector: (row) => row.question,
+  },
+  {
+    name: "Segment Type",
+    selector: (row) => row.segmentType,
+  },
+  {
+    name: "Segment Description",
+    selector: (row) => row.segmentDescription,
+  },
+  {
+    name: "Answer",
+    selector: (row) => row.answer,
+  },
+  {
+    name: "Count",
+    selector: (row) => row.count,
+  },
+  {
+    name: "Percentage",
+    selector: (row) => row.percentage,
+  },
+];
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/questions")
+      .then((res) => {
+        setQuestions(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Start</h1>
+      <DataTable columns={columns} data={questions} />;
     </div>
   );
 }
